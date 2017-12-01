0a05a032a464aaa6d3946d0cb8d78e77d77d69fd
Brett Commandeur <bcmdr.co@gmail.com>
2017-12-01 08:44:53 -0700

    refactor main view into a component

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
-// Assume lib/TimelineCards.js
+// assume import TimelineCards
 
-var cardListVM = new Vue({
-  el: main,
-  data: {
-    cardList: TimelineCards.formattedList
-  },
-  created: function () {
-    console.log(this.cardList)
+Vue.component('ul-card-list', {
+  // TODO refactor card into child component of cardList
+  template: '<ol><li v-for="card in cardList">{{ card.year }} — {{ card.title }}</li></ol>',
+  data: function () {
+    return {
+      cardList: TimelineCards.formattedList
+    }
   }
+})
+
+new Vue({
+  el: "#main",
+  // data: {
+  //   cardList: TimelineCards.formattedList
+  // },
+  // created: function () {
+  //   console.log(this.cardList)
+  // },
 })
